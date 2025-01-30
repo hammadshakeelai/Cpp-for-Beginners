11b94babdcac13ac0cd76a90faf96e519c2a0459
hammadshakeelai <hammadshakeel61@gmail.com>
2025-01-30 18:04:21 +0500

    wow final understood how to use and run it and variablesssss

--- C++/hello world.cpp
+++ C++/hello world.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
 int main() {
-	cout<<"Hello! World.   _/|'-'|\_";
-	return 0;
+    cout << "Hello, World!";
+    return 0;
 }
---
+++ C++/voodoo.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num1;
+    cout << "Enter a number: ";
+    cin >> num1;
+    cout << "Your number is: " << num1 << endl;
+    return 0;
+}
