38195da20718b6552ec3c8cf7df82aeb5e9b4267
hammadshakeelai <hammadshakeel61@gmail.com>
2025-02-09 17:56:11 +0500

    u

---
+++ C++/pr.cpp
@@ -0,0 +1,33 @@
+// #include <iostream>
+// using namespace std;
+// int main() {
+// cout << "****************************" << endl;
+// cout << "* *" << endl;
+// cout << "* Welcome to C++ World! *" << endl;
+// cout << "* *" << endl;
+// cout << "****************************" << endl;
+// cout << endl; // Blank line for spacing
+// cout << "Please choose an option:" << endl;
+// cout << "1. Start a new project" << endl;
+// cout << "2. Load existing project" << endl;
+// cout << "3. Exit" << endl;
+// cout << endl; // Blank line for spacing
+// cout << "============================" << endl;
+// cout << " Thank you for using our " << endl;
+// cout << " program! " << endl;
+// cout << "============================" << endl;
+// return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// int main() {
+// cout << "Line 1: Hello, World!\n";
+// cout << "Line 2: Hello,\tWorld!\n";
+// cout << "Line 3: This is a backslash: \\\n";
+// cout << "Line 4: This is a quote: \"Hello!\"\n";
+// cout << "Line 5:   Carriage return:\rOverwrite\n";
+// cout << "Line 6: Backspace:\bHello\n";
+// cout << "Line 7: Form feed (not visible):\f\fNext Line\f\n\f";
+// cout << "============================" << endl;
+// return 0;
+// }
