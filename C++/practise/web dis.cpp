6cde6124348f2dcf8170b9bc569361d52b5d99b3
hammadshakeelai <hammadshakeel61@gmail.com>
2025-03-04 19:41:13 +0500

    correction1

--- C++/practise/web dis.cpp
+++ C++/practise/web dis.cpp
@@ -4,14 +4,16 @@ int main(){
 	int i = 0;
 	int a = 1;
 	int b = a+9;
+	int enter[4];
 	while(i<5){
 		b=a+9;
 		while(a<=(b)){
 			cout<<"Item No. "<<a<<endl;
 			a++;
 		}
-		int enter;
-		cin>>enter;
+		cin>>enter[i];
+		i++;
+	
 	}
 	
 return 0;
