a808b85cd15da3e9e3e7aca9d9fe91555fd9b932
hammadshakeelai <hammadshakeel61@gmail.com>
2025-08-18 04:09:49 +0500

    Template basics covered

--- C++/main cpp/template practice/main.cpp
+++ C++/main cpp/template practice/main.cpp
@@ -1,18 +1,13 @@
 #include <iostream>
 using namespace std;
 
-template <typename T>
-template <typename U>
-
-T add(T a, U b) {
-    return a + b;
-}
-U add(T a, U b) {
-    return a + b;
+template<typename T>
+void print_default() {
+    T a = 0;
+    cout << a << endl;
 }
 
 int main () {
-    cout << add(5, 10.5) << endl;        // Integer Floating-point addition
-    cout << add(5.5, 10) << endl;    // Floating-point Integer addition
+    print_default<int>();
     return 0;
 }
