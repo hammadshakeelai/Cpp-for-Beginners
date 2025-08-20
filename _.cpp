252fd192cd9f1e51bbfd6b7b002f2133846635f7
hammadshakeelai <hammadshakeel61@gmail.com>
2025-08-20 15:00:53 +0500

    bored

--- _.cpp
+++ _.cpp
@@ -1,9 +1,14 @@
-
-#include <iostream>
-
+#include<iostream>
 using namespace std;
-
-int main() {
-    cout << "Hello, World!" << endl;
+int main(){
+    string number = "42456789";
+    // int num = stoi(number); // The number is: 42
+    int num = 0;
+    for (char ch : number){
+        num *= 10;
+        num += ( ch -48);
+    }
+    cout <<num << endl;
+    // cout << "(48 == '0') === "  << (48 == '0') << endl; // (48 == '0') === 1
     return 0;
 }
