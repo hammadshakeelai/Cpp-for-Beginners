a8c8262d33a2373f741eb131a731db9a081ac01b
hammadshakeelai <hammadshakeel61@gmail.com>
2025-09-04 02:25:55 +0500

    trying out vscode commit system

--- C++/waiz'sFirstCommit.cpp
+++ C++/waiz'sFirstCommit.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 int main() {
     cout << "Hello, World!" << endl;
-    cout << "This is my first commit in C++." << endl;
+    cout << "This is my first commit in C++." << endl;//
     return 0;
 }
